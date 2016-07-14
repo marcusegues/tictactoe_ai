8b85a11786c09dfceeae935b245bca4370874ba6
malans <marcusegues@gmail.com>
2016-07-14 14:48:57 +0200

    Working version

--- game.rb
+++ game.rb
@@ -5,15 +5,80 @@ require 'byebug'
 class Game
   attr_accessor :root
 
-  def game_loop
-    initial_position = gets.chomp.to_i
-    @root = Node.new(Constants.positions[initial_position], 0, :black)
+  def get_initial_user_move
+    initial_position = get_user_input
+    @root = Node.new(0, Constants.positions[initial_position], :white)
     @root.build_tree
-    # puts "#{@root.class.black_count} #{@root.class.white_count} #{@root.class.draw_count}"
   end
+
+  def get_user_input
+    puts "Choose a position: "
+    gets.chomp.to_i
+  end
+
+  def display_initial_game_state
+    board = (0..8).to_a.reverse
+    puts
+    puts "#{board[0]} #{board[1]} #{board[2]}"
+    puts "#{board[3]} #{board[4]} #{board[5]}"
+    puts "#{board[6]} #{board[7]} #{board[8]}"
+    puts
+  end
+
+  def game_loop
+    turn = 0;
+    display_initial_game_state
+    get_initial_user_move
+    until @root.won? || @root.game_over?
+      turn += 1
+      if turn % 2 == 0
+        @root.display_board
+        @root = get_user_next_move
+      else
+        @root = get_ai_next_move
+      end
+    end
+    display_game_result
+  end
+
+  def display_game_result
+    @root.display_board
+    if @root.won?
+      puts "#{@root.color} won"
+    else
+      puts "Draw."
+    end
+  end
+
+  def get_ai_next_move
+    @root.adjacent.each do |next_node|
+      return next_node if next_node.path_to_victory
+    end
+    @root.adjacent.each do |next_node|
+      return next_node if next_node.adjacent.none? { |next_next_node| next_next_node.path_to_victory }
+    end
+  end
+
+  def get_user_next_move
+    next_moves = @root.next_moves
+    loop do
+      next_position = get_user_input
+      next_node_idx = next_moves.find_index(next_position)
+      return @root.adjacent[next_node_idx] if next_node_idx
+    end
+  end
+
+  def calculate_all_possible_games
+    (0..8).to_a.each do |initial_position|
+      @root = Node.new(Constants.positions[initial_position], 0, :black)
+      @root.build_tree
+    end
+    puts "#{@root.class.black_count} #{@root.class.white_count} #{@root.class.draw_count}"
+  end
+
 end
 
-# unless $PROGRAM_NAME == __FILE__
-#   game = Game.new
-#   game.game_loop
-# end
+if $PROGRAM_NAME == __FILE__
+  game = Game.new
+  game.game_loop
+end
--- node.rb
+++ node.rb
@@ -10,8 +10,8 @@ class Node
     attr_accessor :black_count, :white_count, :draw_count
   end
 
-  attr_accessor :black, :white, :path_to_victory, :adjacent
-  def initialize(black = 0, white = 0, color = :black, adjacent = [])
+  attr_accessor :black, :white, :path_to_victory, :adjacent, :color
+  def initialize(black = 0, white = 0, color = :white, adjacent = [])
     @black = black
     @white = white
     @color = color
@@ -32,10 +32,10 @@ class Node
   def display_board
     board = Array.new(9) {"_"}
     binary_representation(@black).split("").each_with_index do |el, i|
-      board[i] = "o" if el == "1"
+      board[i] = "x" if el == "1"
     end
     binary_representation(@white).split("").each_with_index do |el, i|
-      board[i] = "x" if el == "1"
+      board[i] = "o" if el == "1"
     end
     puts "#{board[0]} #{board[1]} #{board[2]}"
     puts "#{board[3]} #{board[4]} #{board[5]}"
@@ -45,6 +45,7 @@ class Node
 
   def build_tree
     # display_board
+    paths_to_victory_on_next_current_player_move = nil
     unless @won || game_over?
       paths_to_victory_on_next_current_player_move = []
       next_moves.each do |move|
@@ -61,7 +62,11 @@ class Node
     if @won
       @path_to_victory = true
     else
-      @path_to_victory = paths_to_victory_on_next_current_player_move.all?
+      if paths_to_victory_on_next_current_player_move
+        @path_to_victory = paths_to_victory_on_next_current_player_move.all?
+      else
+        @path_to_victory = false
+      end
     end
 
     update_game_counts
